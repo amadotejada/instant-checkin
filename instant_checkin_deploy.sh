869e2410852d5f1bb116b2dcf3d7ac46b98db51c
Amado Tejada <44622125+amadotejada@users.noreply.github.com>
2024-01-19 01:17:15 -0500

    Update instant_checkin_deploy.sh

--- instant_checkin_deploy.sh
+++ instant_checkin_deploy.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
+# amado.tejada | 01-19-2024
 
 if [[ $(id -u) -ne 0 ]] ; then echo "Run as root" ; exit 1 ; fi
 
 DOMAIN=""
 
-if [[ ! $DOMAIN ]]; then echo "\$DOMAIN not set"; exit 1; fi
+if [[ ! $DOMAIN ]]; then echo "$DOMAIN not set"; exit 1; fi
 
 cat << EOF > "/Library/LaunchDaemons/com.$DOMAIN.instant_checkin.plist"
 <?xml version="1.0" encoding="UTF-8"?>
@@ -13,52 +14,60 @@ cat << EOF > "/Library/LaunchDaemons/com.$DOMAIN.instant_checkin.plist"
 <plist version="1.0">
 <dict>
 	<key>Label</key>
-	<string>com.$DOMAIN.instant_checkin</string>
+	<string>com.\$DOMAIN.instant_checkin</string>
 	<key>ProgramArguments</key>
 	<array>
 		<string>sh</string>
-		<string>/Library/Application Support/$DOMAIN/instant_checkin_runner.sh</string>
+		<string>/Library/Application Support/\$DOMAIN/instant_checkin_runner.sh</string>
 	</array>
 	<key>WatchPaths</key>
 	<array>
-		<string>/Library/Managed Preferences/com.$DOMAIN.instant_checkin.plist</string>
+		<string>/Library/Managed Preferences/com.\$DOMAIN.instant_checkin.plist</string>
 	</array>
 </dict>
 </plist>
 EOF
 
 mkdir -p "/Library/Application Support/$DOMAIN"
+rm -f "/Library/Application Support/$DOMAIN/instant_checkin_runner.sh"
 
 cat << EOF > "/Library/Application Support/$DOMAIN/instant_checkin_runner.sh"
 #!/bin/bash
 
+# amado.tejada | 01/19/2024
+
+echo "\nStarted: Instant checkin run: \$(date)" >>/var/log/INSTANT_CHECKIN.log
+/usr/local/bin/jamf policy -id 241
 {
-if [[ -e "/Library/Managed Preferences/com.$DOMAIN.instant_checkin.plist" ]]; then
-	policyid=\$(defaults read "/Library/Managed Preferences/com.$DOMAIN.instant_checkin.plist" policyid)
-	if [[ \$policyid =~ ^[0-9]+$ ]]
-	then
-		echo "policyid \$policyid"
-		/usr/local/bin/jamf policy -id \$policyid
-	else
-		echo "normal checkin"
-		/usr/local/bin/jamf policy
+	if [ -e "/Library/Managed Preferences/com.\$DOMAIN.instant_checkin.plist" ]; then
+		policyid=\$(defaults read "/Library/Managed Preferences/com.\$DOMAIN.instant_checkin.plist" policyid)
+		if [[ "\$policyid" =~ ^[0-9]+$ ]]; then
+			echo "Running: Policy \$policyid + recon: \$(date)" >>/var/log/INSTANT_CHECKIN.lo
+			/usr/local/bin/jamf policy -id \$policyid
+		else
+			echo "Running: Normal policies + recon: \$(date)" >>/var/log/INSTANT_CHECKIN.log
+			/usr/local/bin/jamf policy
+		fi
 	fi
 
-/usr/local/bin/jamf recon -room "Instant Check-in Ready! Run by changing this field to: check-in"
-
-fi
-
 } || {
-	echo "error, resetting jamf instant check-in"
-    /usr/local/bin/jamf recon -room "Instant Check-in Ready! Run by changing this field to: check-in"
+	echo "Error: No instant_checkin.plist file found: \$(date)" >>/var/log/INSTANT_CHECKIN.log
 }
 
+/usr/local/bin/jamf recon
+echo "Ended: Instant checkin run: \$(date)" >>/var/log/INSTANT_CHECKIN.log
 EOF
 
 chmod +x "/Library/Application Support/$DOMAIN/instant_checkin_runner.sh"
 chown root:wheel /Library/LaunchDaemons/com.$DOMAIN.instant_checkin.plist
 chmod 644 /Library/LaunchDaemons/com.$DOMAIN.instant_checkin.plist
+launchctl unload /Library/LaunchDaemons/com.$DOMAIN.instant_checkin.plist
 launchctl load -w /Library/LaunchDaemons/com.$DOMAIN.instant_checkin.plist
-jamf recon -room "Instant Check-in Ready! Run by changing this field to: check-in"
 
-echo -e "\033[0;32m\nInstant Check-in: Ready\n\033[0m"
+echo -e "\nInstalled: instant_checkin_runner.sh: $(date)" >>/var/log/INSTANT_CHECKIN.log
+/usr/local/bin/jamf policy -id 241
+/usr/local/bin/jamf recon
+
+echo -e "\033[0;32m\nInstant Check-in: Ready\033[0m"
+
+exit 0
